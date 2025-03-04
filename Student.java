21ef3b6583edf5b713613b93146ac3c7a2d2a4f8
Nidhi Supe <161975784+NidhiSupe@users.noreply.github.com>
2025-03-04 21:21:49 +0530

    Creating Student class

---
+++ Student.java
@@ -0,0 +1,75 @@
+// Student.java
+public class Student {
+
+    private int prn;
+    private String name;
+    private String dob;
+    private double marks;
+    private int position;
+
+    // Constructors
+    public Student(int prn) {
+        this.prn = prn;
+    }
+
+    public Student(int prn, String name) {
+        this(prn);
+        this.name = name;
+    }
+
+    public Student(int prn, String name, double marks) {
+        this(prn, name);
+        this.marks = marks;
+    }
+
+    public Student(int prn, String name, double marks, String dob) {
+        this(prn, name, marks);
+        this.dob = dob;
+    }
+
+    // Getters and Setters
+    public int getPrn() {
+        return prn;
+    }
+
+    public void setPrn(int prn) {
+        this.prn = prn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [prn=" + prn + ", name=" + name + ", dob=" + dob + ", marks=" + marks + ", position=" + position + "]";
+    }
+}
